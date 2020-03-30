85755fe620e81142689a2dc45e63e8408cf330ab
Patrick Duque <patpatduque26@gmail.com>
2020-03-30 21:54:12 +0800

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath : process.env.NODE_ENV === 'production' ? '/' + process.env.smart - brain + '/' : '/'
+	publicPath : process.env.NODE_ENV === 'production' ? '/' + 'smart-brain' + '/' : '/'
 };
